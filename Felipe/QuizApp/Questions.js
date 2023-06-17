b609eb864287537061e59ea9c2f5abb56eca5bc5
GabiGomes98 <127056206+GabiGomes98@users.noreply.github.com>
2023-06-17 17:59:34 -0300

    Create Questions.js

---
+++ Felipe/QuizApp/Questions.js
@@ -0,0 +1,102 @@
+export default [
+    {
+        question: "Pergunta 1: Qual é a capital do Brasil?",
+        options: {
+          A: "Brasília",
+          B: "São Paulo",
+          C: "Rio de Janeiro",
+          D: "Belo Horizonte"
+        },
+        answer: "A"
+      },
+      {
+        question: "Questão 02: A didática tem como objeto de estudo:",
+        options: {
+          A: "As ações docentes restritas ao Ensino Fundamental.",
+          B: "A teoria dissociada da prática docente.",
+          C: "A legislação educacional nos anos finais da Educação Infantil.",
+          D: "O processo de ensino na sua globalidade."
+        },
+        answer: "D"
+      },
+      {
+        question: "Questão 03: Qual das alternativas abaixo apenas contêm classes de palavras?",
+        options: {
+          A: "Vogais, semivogais e consoantes",
+          B: "Fonologia, Morfologia e Sintaxe",
+          C: "Substantivo, verbo e preposição",
+          D: "Artigo, verbo transitivo e verbo intransitivo."
+        },
+        answer: "C"
+      },
+      {
+        question: "Questão 04: Quais as duas datas que são comemoradas em novembro?",
+        options: {
+          A: "Independência do Brasil e Dia da Bandeira",
+          B: "Proclamação da República e Dia Nacional da Consciência Negra",
+          C: "Dia do Médico e Dia de São Lucas",
+          D: "Dia de Finados e Dia Nacional do Livro."
+        },
+        answer: "B"
+      },
+      {
+        question: "Questão 05: O que a palavra legend significa em português?",
+        options: {
+          A: "Legenda",
+          B: "Conto",
+          C: "História",
+          D: "Lenda"
+        },
+        answer: "D"
+      },
+      {
+        question: "Questão 06: Qual o livro mais vendido no mundo a seguir da Bíblia?",
+        options: {
+          A: "O Senhor dos Anéis",
+          B: "Dom Quixote",
+          C: "O Pequeno Príncipe",
+          D: "Um Conto de Duas Cidades"
+        },
+        answer: "B"
+      },
+      {
+        question: "Questão 07: De quem é a famosa frase “Penso, logo existo”?",
+        options: {
+          A: "Platão",
+          B: "Descartes",
+          C: "Sócrates",
+          D: "Francis Bacon"
+        },
+        answer: "B"
+      },
+      {
+        question: "Questão 08: Em que período da pré-história o fogo foi descoberto?",
+        options: {
+          A: "Neolítico",
+          B: "Paleolítico",
+          C: "Idade dos Metais",
+          D: "Idade Média"
+        },
+        answer: "B"
+      },
+      {
+        question: "Questão 09: Qual era o nome de Aleijadinho?",
+        options: {
+          A: "Antônio Francisco Lisboa",
+          B: "Francisco Antônio Lisboa",
+          C: "Manuel Francisco Lisboa",
+          D: "Alex Francisco Lisboa"
+        },
+        answer: "A"
+      },
+      {
+        question: "Questão 10: Quais destas doenças são sexualmente transmissíveis?",
+        options: {
+          A: "Aids, tricomoníase e ebola",
+          B: "Chikungunya, aids e herpes genital",
+          C: "Gonorreia, clamídia e sífilis",
+          D: "Hepatite B, febre tifoide e hanseníase"
+        },
+        answer: "C"
+      }
+    ];
